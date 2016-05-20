757713fc13df741fe1ecf94505503a49a5135648
theDoctor <steve.kyle@dcci.local>
2016-05-20 10:30:45 -0400

    mod deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,60 @@
-#!/usr/bin/env bash
-BRANCH=master
-TARGET_REPO=stkyle/stkyle.github.io.git
-PELICAN_OUTPUT_FOLDER=output
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 
-echo -e "Testing travis-encrypt"
-echo -e "$VARNAME"
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+PELICAN_OUPTPUT_DIR=output
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
-    if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "steve.kyle@gmail.com"
-        git config --global user.name "stkyle"
-    fi
-    #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://stkyle@github.com/$TARGET_REPO built_website > /dev/null
-    #go into directory and copy data we're interested in to that directory
-    cd built_website
-    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
-    #add, commit and push files
-    git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq origin $BRANCH > /dev/null
-    echo -e "Deploy completed\n"
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    #npm run spec
+    exit 0
 fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO output
+cd output
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+# If there are no changes (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the spec on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
